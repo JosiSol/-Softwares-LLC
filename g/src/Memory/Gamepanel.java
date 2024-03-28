be35a9de8915a4266779a840790764485d393ced
Hyoketsu_Senpai <youseftela@gmail.com>
2024-03-28 20:47:29 +0300

    changed things here and there

--- g/src/Memory/Gamepanel.java
+++ g/src/Memory/Gamepanel.java
@@ -11,14 +11,15 @@ public class Gamepanel extends JFrame{
     private final String GAME_NAME = "Memory Game";
     private final int WIDTH = 1000;
     private final int HEIGHT = 600;
-    private final String ICON_PIC = "Assets\\gameicon.png";
+    private final String ICON_PIC = "g\\Assets\\gameicon.png";
 
+    //static {Gamepanel.setDefaultLookAndFeelDecorated(true);}
     {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle(GAME_NAME);
         this.setSize(WIDTH, HEIGHT);
         //dont set it to any color this is just for testing
-        this.getContentPane().setBackground(getBackground());
+        this.getContentPane().setBackground(Color.ORANGE);
         this.setIconImage(new ImageIcon(ICON_PIC).getImage());
         //this.pack(); resizes based on components might use later
         this.setLocationByPlatform(true);
