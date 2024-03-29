773b25d897783f05e8dfafcdd1bfcf26965c5696
yabqal <yabqal@duck.com>
2024-03-29 06:53:35 +0300

    Code Reformatting

--- g/src/Memory/Gamepanel.java
+++ g/src/Memory/Gamepanel.java
@@ -11,19 +11,24 @@ public class Gamepanel extends JFrame{
     private final String GAME_NAME = "Memory Game";
     private final int WIDTH = 1000;
     private final int HEIGHT = 600;
-    private final String ICON_PIC = "g\\Assets\\gameicon.png";
+    private final String ICON_PIC = "/../Assets/gameicon.png";
 
     //static {Gamepanel.setDefaultLookAndFeelDecorated(true);}
     {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle(GAME_NAME);
         this.setSize(WIDTH, HEIGHT);
+
         //dont set it to any color this is just for testing
         this.getContentPane().setBackground(Color.ORANGE);
-        this.setIconImage(new ImageIcon(ICON_PIC).getImage());
+
+        //instead of this.setIconImage(new ImageIcon(ICON_PIC).getImage());
+        ImageIcon icon = new ImageIcon(ICON_PIC);
+        this.setIconImage(icon.getImage());
+
         //this.pack(); resizes based on components might use later
         this.setLocationByPlatform(true);
         this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     Gamepanel(){}
