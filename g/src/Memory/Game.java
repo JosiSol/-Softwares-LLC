c03c4d56670ec3ab425f5d3e0b2759fe6e961668
Hyoketsu_Senpai <youseftela@gmail.com>
2024-04-13 06:55:56 +0300

    Added a semi-functional about us page. Much formatting is needed though.

--- g/src/Memory/Game.java
+++ g/src/Memory/Game.java
@@ -3,10 +3,9 @@ package Memory;
 //main class that will handle the interaction between the game objects/components
 public class Game {
 	
-	GamePanel gamePanel = new GamePanel();
-	
     public static void main(String[] args)
     {   
+        AboutUs aboutUs = new AboutUs();
         GameMenu gameMenu = new GameMenu();
         CompanyLogo companyLogo = new CompanyLogo();
         GamePanel gamePanel = new GamePanel();
@@ -18,10 +17,33 @@ public class Game {
         gamePanel.getContentPane().remove(companyLogo);
         gamePanel.setResizable(true);
         
-        
-        gamePanel.getContentPane().add(gameMenu);// button wont be visibile after adding unless we refresh the screen or set the bounds
-        System.out.println("done");
-        gamePanel.pack();
+        while(true){
+
+            gamePanel.getContentPane().add(gameMenu);// button wont be visibile after adding unless we refresh the screen or set the bounds   
+            gamePanel.repaint();
+            gamePanel.pack();
+            while(!GameMenu.showAboutUsPage){}//waste timer till user makes a choice
+            gamePanel.getContentPane().remove(gameMenu);
+            System.out.println("rightt");
+            if(GameMenu.showAboutUsPage)
+            {
+                // add panel that holds thingys
+                gamePanel.getContentPane().add(aboutUs);
+                gamePanel.repaint();
+                gamePanel.pack();
+                System.out.println("right");
+                while(GameMenu.showAboutUsPage){}/*back button wasn't clicked*/
+                gamePanel.remove(aboutUs);
+                
+            }
+            else //if game start 
+            {
+                System.out.println("wrong");
+                //start game
+                //while exit wasnt clicked
+            }
+
+    }
 
     }
 }
--- g/src/Memory/GameMenu.java
+++ g/src/Memory/GameMenu.java
@@ -1,51 +1,63 @@
 package Memory;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.FontMetrics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 //game menu that will have options you can choose from when start the program
 public class GameMenu extends JPanel implements MouseListener{
-
+    //make into variables what needs to become variables/constants
     private final int WIDTH = 1000;
     private final int HEIGHT = 600;
-    JLabel startGame = new JLabel("Start");
-    JLabel exit = new JLabel("Exit");
+    //we could use images instead of text cuz cooler font and whatnot
+    private JLabel startGame = new JLabel("Start");
+    private JLabel exit = new JLabel("Exit");
+    private JLabel aboutUs =  new JLabel("About us");
+    public static volatile boolean showAboutUsPage;
     
-        
+      
     {
+        GameMenu.showAboutUsPage = false;
         this.setLayout(new FlowLayout(0,100,10));
         this.setBackground(Color.blue);
         this.setOpaque(true);
         this.setPreferredSize(new Dimension(WIDTH,HEIGHT));//since the game panel has a boarder layout it will be resized to full screen either way
-
+        //this.setVisible(true);
         
         startGame.addMouseListener(this);
         exit.addMouseListener(this);
+        aboutUs.addMouseListener(this);
 
+        //the widths arent random.
+        //fontmetrics were used to figure out the correct widths(at least for the most part)
         startGame.setPreferredSize(new Dimension(70, 30));
-        exit.setPreferredSize(new Dimension(70, 30));
-
+        exit.setPreferredSize(new Dimension(56, 30));
+        aboutUs.setPreferredSize(new Dimension(131, 30));
+        
         startGame.setFont(startGame.getFont().deriveFont(20.0f));
         exit.setFont(startGame.getFont().deriveFont(20.0f));
+        aboutUs.setFont(aboutUs.getFont().deriveFont(20.0f));
+
+        
 
         this.add(startGame);
+        this.add(aboutUs);
         this.add(exit);
         
         
         //set visibility true only after adding
+        //might be automatically set to true for jlabels dunno
         startGame.setVisible(true);
         exit.setVisible(true);
+        aboutUs.setVisible(true);
     }
-
+    
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -58,16 +70,22 @@ public class GameMenu extends JPanel implements MouseListener{
                 System.exit(0);
             }
         }
-        
+        if(e.getSource() == aboutUs)
+        {
+            GameMenu.showAboutUsPage = true;
+        }  
+        System.out.println(GameMenu.showAboutUsPage);
     }
 
-
+    //change the size of the label when you change the size of the font/text
     @Override
     public void mousePressed(MouseEvent e) {
         if(e.getSource() == startGame)
         startGame.setFont(startGame.getFont().deriveFont(10.0f));
         if(e.getSource() == exit)
         exit.setFont(startGame.getFont().deriveFont(10.0f));
+        if(e.getSource() == aboutUs)
+        aboutUs.setFont(startGame.getFont().deriveFont(10.0f));
 
     }
 
@@ -78,24 +96,32 @@ public class GameMenu extends JPanel implements MouseListener{
         startGame.setFont(startGame.getFont().deriveFont(30.0f));
         if(e.getSource() == exit)
         exit.setFont(startGame.getFont().deriveFont(30.0f));
+        if(e.getSource() == aboutUs)
+        aboutUs.setFont(startGame.getFont().deriveFont(30.0f));
     }
 
 
     @Override
     public void mouseEntered(MouseEvent e) {
         if(e.getSource() == startGame)
-        startGame.setFont(startGame.getFont().deriveFont(30.0f));
+            startGame.setFont(startGame.getFont().deriveFont(30.0f));
         if(e.getSource() == exit)
-        exit.setFont(startGame.getFont().deriveFont(30.0f));
+            exit.setFont(startGame.getFont().deriveFont(30.0f));
+        if(e.getSource() == aboutUs)
+            aboutUs.setFont(startGame.getFont().deriveFont(30.0f));
+        
     }
 
 
     @Override
     public void mouseExited(MouseEvent e) {
         if(e.getSource() == startGame)
-        startGame.setFont(startGame.getFont().deriveFont(20.0f));
+            startGame.setFont(startGame.getFont().deriveFont(20.0f));
         if(e.getSource() == exit)
-        exit.setFont(startGame.getFont().deriveFont(20.0f));
+            exit.setFont(startGame.getFont().deriveFont(20.0f));
+        if(e.getSource() == aboutUs)
+            aboutUs.setFont(startGame.getFont().deriveFont(20.0f));
         
     }
+    public GameMenu(){}
 }
