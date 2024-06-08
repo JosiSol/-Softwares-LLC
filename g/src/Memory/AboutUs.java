4922060c767bb48616dbceadc0a199f9db19d83f
Yosef Solomon <yosefsolomon21@gmail.com>
2024-06-08 23:18:28 +0300

    Step by Step

--- g/src/Memory/AboutUs.java
+++ g/src/Memory/AboutUs.java
@@ -1,39 +1,37 @@
 package Memory;
 
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.Image;
-import java.awt.Toolkit;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class AboutUs extends JPanel implements MouseListener{
-    JLabel tri;
+    JLabel tri,label,label2;
+    ImageIcon icon = new ImageIcon("Assets/backButton.png");
     Toolkit tk = Toolkit.getDefaultToolkit();
-    Image newCur1 = tk.getImage("g/Assets/cursorMain.png");
+    Image newCur1 = tk.getImage("Assets/cursorMain.png");
     {
-        Cursor mainCursor = tk.createCustomCursor(newCur1, getLocation(), TOOL_TIP_TEXT_KEY);
+        Cursor mainCursor = tk.createCustomCursor(newCur1,getLocation(),TOOL_TIP_TEXT_KEY);
         this.setCursor(mainCursor);
         this.setPreferredSize(new Dimension(1200,600));
-        this.setVisible(true);
-        this.setBackground(Color.red);
-        tri = new JLabel("Back");
+        tri = new JLabel(icon);
+        label = new JLabel("Back");
+        tri.setBounds(30, 18, 25, 25);
+        label.setBounds(32,18,25,25);
         tri.addMouseListener(this);
+        label.addMouseListener(this);
         this.add(tri);
+        this.add(label);
         this.add(new JLabel("                                               "));
-        this.add(new JLabel("Memory Game Developed By "));
-        this.add(new JLabel("1. Yosefe Tilahun  UGR/9673/15"));
-        this.add(new JLabel("2. Yabqal Brook    UGR/2822/15"));
-        this.add(new JLabel("3. Yohannes Ketema UGR/6290/15"));
-        this.add(new JLabel("4. Natnael Mesfin  UGR/8654/15"));
-        this.add(new JLabel("5. Yosef Solomon   UGR/7358/15"));
-        this.add(new JLabel("6. Mikiyas Fasil   UGR/9231/15"));
+        this.add(new JLabel("Memory Game Developed By:   "));
+        this.add(new JLabel("1. Yosefe Tilahun  UGR/9673/15     "));
+        this.add(new JLabel("2. Yabqal Brook    UGR/2822/15     "));
+        this.add(new JLabel("3. Yohannes Ketema UGR/6290/15     "));
+        this.add(new JLabel("4. Natnael Mesfin  UGR/8654/15     "));
+        this.add(new JLabel("5. Yosef Solomon   UGR/7358/15     "));
+        this.add(new JLabel("6. Mikiyas Fasil   UGR/9231/15     "));
     }
-
     @Override
     public void mouseClicked(MouseEvent e){
         GameMenu.showAboutUsPage = false;
